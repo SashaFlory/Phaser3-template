359f96b21fa5bbe1d3b147d4d1b3873197019b8c
unknown <sashaflory90@gmail.com>
2023-04-18 17:03:43 -0300

    game, gameover

---
+++ assets/scenes/Game.js
@@ -0,0 +1,14 @@
+export default class Game extends Phaser.Scene {
+    constructor() {
+      super("Game");
+    }
+  
+    init() {}
+  
+    preload() {}
+  
+    create() {}
+  
+    update() {}
+  }
+  
---
+++ assets/scenes/GameOver.js
@@ -0,0 +1,14 @@
+export default class GameOver extends Phaser.Scene {
+    constructor() {
+      super("GameOver");
+    }
+  
+    init() {}
+  
+    preload() {}
+  
+    create() {}
+  
+    update() {}
+  }
+  
